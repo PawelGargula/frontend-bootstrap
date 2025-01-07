fe97b397fb834205d2c8c629d9b69704370e27e8
Paweł Gargula <75017679+PawelGargula@users.noreply.github.com>
2025-01-07 20:08:47 +0100

    Env (#5)
    * Refactor CI workflows to separate Netlify and Vercel deployment jobs
    
    * Add footer component and update environment variable handling in CI workflows
    
    * Fix environment variable handling in Vercel and Netlify deployment workflows
    
    * Restore VITE_ENVIRONMENT variable in Vercel deployment workflows
    
    * Update footer component to use imported logo source
    
    * Remove conditional rendering of Logo component in Footer
    
    * Add VITE_ENVIRONMENT variable to pull request workflow
    
    * Add VITE_ENVIRONMENT variable to Vercel deployment workflows
    
    * Remove VITE_ENVIRONMENT variable from Vercel deployment actions and add it to pull request workflow
    
    * Re-enable conditional rendering of Logo component in Footer based on VITE_ENVIRONMENT variable
    
    * Update VITE_ENVIRONMENT variable usage in deployment workflows for Netlify and Vercel

---
+++ src/components/footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useConfig } from '../context/ConfigContext';
+import logoSrc from '../../_resources/img/logo-dark-vertical-1.png';
+
+export default function Footer() {
+    const { appVersion } = useConfig();
+
+    if (!import.meta.env.VITE_ENVIRONMENT) {
+        return <Logo />;
+    }
+
+    console.log(import.meta.env);
+
+    return (
+        <div className="flex justify-between items-center mt-4">
+            <div className="w-40"></div>
+            <Logo />
+            <div className="text-center text-sm text-gray-500 bg-white p-2 rounded-lg shadow-md">
+                ENV: {import.meta.env.VITE_ENVIRONMENT} - VER: {appVersion}
+            </div>
+        </div>
+  )
+}
+
+function Logo() {
+    return <img src={logoSrc} alt="Logo projektu Opanuj Frontend" className="w-32 mx-auto" />;
+}
